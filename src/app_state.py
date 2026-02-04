f9647ceb90329d6c128dba1f87bca4b1275203d2
Nicolas Reyrolle <405928+NicolasReyrolle@users.noreply.github.com>
2026-02-04 11:40:29 +0100

    Exclude the ui elements from the reset method

--- src/app_state.py
+++ src/app_state.py
@@ -10,13 +10,14 @@ class AppState:
 
     def __init__(self) -> None:
         self.reset()
+        self.input_file: ui.input
+        self.log: ui.log
 
     def reset(self) -> None:
         """Reset the application state."""
         self.workouts: WorkoutManager = WorkoutManager()
         self.file_loaded: bool = False
-        self.input_file: ui.input
-        self.log: ui.log
+
 
         self.metrics = {
             "count": 0,
