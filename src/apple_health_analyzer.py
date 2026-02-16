1c38a2dd974e78178915ad87013f1e9401dc81e6
Nicolas Reyrolle <405928+NicolasReyrolle@users.noreply.github.com>
2026-02-16 15:17:39 +0100

    Do not automatically open the browser

--- src/apple_health_analyzer.py
+++ src/apple_health_analyzer.py
@@ -149,4 +149,5 @@ if __name__ in {"__main__", "__mp_main__"}:
         favicon=APP_ICON_BASE64,
         storage_secret=secret,
         uvicorn_reload_dirs="src,resources",  # Only include needed dirs for the reload
+        show=False,
     )
